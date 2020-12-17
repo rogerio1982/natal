f904998a30b507194ee171fee13a074aa415845f
Rogério Soares <rogerio_si@yahoo.com.br>
2020-12-17 18:17:32 -0300

    Add files via upload

---
+++ natal.py
@@ -0,0 +1,22 @@
+import random
+
+numberList = ["@","#"]
+#print("random item from list is: ", random.choice(numberList))
+
+linhas = 20
+tamanho = linhas * 2 - 10
+espacos = (tamanho - 1) / 2
+i = 1
+ 
+while i <= linhas:
+#print *
+   print (""," " * (20 - i + 1), random.choice(numberList)  * (2 * i - 1))
+   i = i + 1
+    
+#print ##
+i=1
+while i <= 20:
+   print (""," " * (20 - i + 1), "##"  * (2 * i - 1))
+   i = i + 1
+   break
+print ("Feliz Natal!")
